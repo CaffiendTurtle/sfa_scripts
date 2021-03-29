e0b070e46822aabbe640f4ade1ec0674effd331b
CaffiendTurtle <ep041998@gmail.com>
2021-03-28 23:19:14 -0500

    Set Up for QT and instantiate the Simple UI class. It becomes an instance of this class. So from here we can call UI as an object.

---
+++ src/simpleui.py
@@ -0,0 +1,12 @@
+from PySide2 import QtWidgets
+
+
+class SimpleUI(QtWidgets.QDialog):
+
+
+    def __init__(self):
+
+
+
+        super(SimpleUI, self).__init__()
+        self.setWindowTitle("A Simple UI")
