0518e34c4fef93d72a94bac3d2352c19a5aba828
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 02:43:28 -0500

    imported random so that I could create random rotations in all 3 axis for each instance. They are placed inside one function currently and still needed to be placed in their own function. Am having confusion on how to create a for loop for it as well as how to limit the selection of what axis should be rotated.

--- src/scatter.py
+++ src/scatter.py
@@ -1,13 +1,16 @@
 import logging
 
 from PySide2 import QtWidgets, QtCore, QtGui
+import random
 import maya.cmds as cmds
+
+
+# def ran_rotation():
 
 
 def place_instances():
     selected_geo = cmds.ls(orderedSelection=True, flatten=True)
     selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
-
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -15,3 +18,10 @@ def place_instances():
         position = cmds.pointPosition(vert)
         # place the newly created instances onto the vertice positions
         cmds.move(position[0], position[1], position[2], new_instance)
+        # creates random rotations for each instance created
+        # need to create its own function so that I can just place that in
+        xRot = random.uniform(0, 360)
+        yRot = random.uniform(0, 360)
+        zRot = random.uniform(0, 360)
+        cmds.rotate(xRot, yRot, zRot, new_instance)
+
