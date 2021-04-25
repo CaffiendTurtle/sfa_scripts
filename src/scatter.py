d52d3b47c43b7f566a04c203f8e993968403e80b
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 06:10:26 -0500

    I have placed the scale and rotation into their own functions however it is not allowing me to place the range once but in fact does it for every instance. This needs to be figured out.

--- src/scatter.py
+++ src/scatter.py
@@ -5,23 +5,33 @@ import random
 import maya.cmds as cmds
 
 
-# def ran_rotation(new_instance):
-
-
-# def ran_scaling():
-
-
-def place_instances():
-    selected_geo = cmds.ls(orderedSelection=True, flatten=True)
-    selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
+def ran_rotation(new_instance):
     minX_rotation = int(input())
     maxX_rotation = int(input())
     minY_rotation = int(input())
     maxY_rotation = int(input())
     minZ_rotation = int(input())
     maxZ_rotation = int(input())
-    min_scale = int(input())
-    max_scale = int(input())
+    xRot = random.uniform(minX_rotation, maxX_rotation)
+    yRot = random.uniform(minY_rotation, maxY_rotation)
+    zRot = random.uniform(minZ_rotation, maxZ_rotation)
+    cmds.rotate(xRot, yRot, zRot, new_instance)
+
+
+def ran_scaling(new_instance):
+     # allows user to set their own min max scale
+     # needs to be placed in its own function and needs to be a for loop
+     min_scale = int(input())
+     max_scale = int(input())
+     scaling_Factor = random.uniform(min_scale, max_scale)
+     cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor, new_instance)
+
+
+def place_instances():
+    selected_geo = cmds.ls(orderedSelection=True, flatten=True)
+    selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
+    # min_scale = int(input())
+    # max_scale = int(input())
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -32,13 +42,5 @@ def place_instances():
         # creates random rotations for each instance created
         # user can create min max values for their rotation range per axis
         # need to create its own function so that I can just place that in
-        xRot = random.uniform(minX_rotation, maxX_rotation)
-        yRot = random.uniform(minY_rotation, maxY_rotation)
-        zRot = random.uniform(minZ_rotation, maxZ_rotation)
-        cmds.rotate(xRot, yRot, zRot, new_instance)
-        # creates random scaling for each instance based upon a hard code range
-        # needs to be placed in its own function and needs to be a for loop
-        # need the range to be selective
-        scaling_Factor = random.uniform(min_scale, max_scale)
-        cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor,
-                   new_instance)
+        ran_rotation(new_instance)
+        ran_scaling(new_instance)
