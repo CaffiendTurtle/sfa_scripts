03c60cca1ee7ab0573b248b3a1b292d6d549b249
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 04:32:22 -0500

    Better named variables for min max range of each axis rotation

--- src/scatter.py
+++ src/scatter.py
@@ -20,12 +20,12 @@ import maya.cmds as cmds
 def place_instances():
     selected_geo = cmds.ls(orderedSelection=True, flatten=True)
     selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
-    fx_select = int(input())
-    sx_select = int(input())
-    fy_select = int(input())
-    sy_select = int(input())
-    fz_select = int(input())
-    sz_select = int(input())
+    minX_select = int(input())
+    maxX_select = int(input())
+    minY_select = int(input())
+    maxY_select = int(input())
+    minZ_select = int(input())
+    maxZ_select = int(input())
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -34,10 +34,11 @@ def place_instances():
         # place the newly created instances onto the vertice positions
         cmds.move(position[0], position[1], position[2], new_instance)
         # creates random rotations for each instance created
+        # user can create min max values for their rotation range per axis
         # need to create its own function so that I can just place that in
-        xRot = random.uniform(fx_select, sx_select)
-        yRot = random.uniform(fy_select, sy_select)
-        zRot = random.uniform(fz_select, sz_select)
+        xRot = random.uniform(minX_select, maxX_select)
+        yRot = random.uniform(minY_select, maxY_select)
+        zRot = random.uniform(minZ_select, maxZ_select)
         cmds.rotate(xRot, yRot, zRot, new_instance)
         # creates random scaling for each instance based upon a hard code range
         # needs to be placed in its own function and needs to be a for loop
