eeb7cb5b68fecbd6fe5d20d48f0aa92dbcf5dae8
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 17:01:46 -0500

    Rotation inputs are now only asked once and applied to every instance. Trying to do the same with scale however it refuses to acknowledge one global varible.

--- src/scatter.py
+++ src/scatter.py
@@ -5,24 +5,32 @@ import random
 import maya.cmds as cmds
 
 
-def ran_rotation(new_instance):
+def user_input():
     minX_rotation = int(input())
     maxX_rotation = int(input())
     minY_rotation = int(input())
     maxY_rotation = int(input())
     minZ_rotation = int(input())
     maxZ_rotation = int(input())
+    min_scale = int(input())
+    max_scale = int(input())
+
+    return minX_rotation, maxX_rotation, minY_rotation, maxY_rotation, minZ_rotation, maxZ_rotation, min_scale, max_scale
+
+def ran_rotation(new_instance,minX_rotation, maxX_rotation,
+                 minY_rotation, maxY_rotation,
+                 minZ_rotation, maxZ_rotation):
+
     xRot = random.uniform(minX_rotation, maxX_rotation)
     yRot = random.uniform(minY_rotation, maxY_rotation)
     zRot = random.uniform(minZ_rotation, maxZ_rotation)
     cmds.rotate(xRot, yRot, zRot, new_instance)
 
 
-def ran_scaling(new_instance):
+def ran_scaling(new_instance, min_scale, max_scale):
      # allows user to set their own min max scale
      # needs to be placed in its own function and needs to be a for loop
-     min_scale = int(input())
-     max_scale = int(input())
+
      scaling_Factor = random.uniform(min_scale, max_scale)
      cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor, new_instance)
 
@@ -32,6 +40,7 @@ def place_instances():
     selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
     # min_scale = int(input())
     # max_scale = int(input())
+    user_input()
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -42,5 +51,9 @@ def place_instances():
         # creates random rotations for each instance created
         # user can create min max values for their rotation range per axis
         # need to create its own function so that I can just place that in
-        ran_rotation(new_instance)
-        ran_scaling(new_instance)
+        ran_rotation(new_instance, minX_rotation, maxX_rotation,
+                     minY_rotation, maxY_rotation,
+                     minZ_rotation, maxZ_rotation)
+        #ran_scaling(new_instance, min_scale,max_scale)
+
+
