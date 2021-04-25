bfb62cb9edd9313b1bf63dcadd8b2baa0915235a
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 01:41:51 -0500

    Made major changes to the place_instances function so that it would create an instance of the first selected geometry rather than be hard coded into a string variable.

--- src/scatter.py
+++ src/scatter.py
@@ -3,28 +3,15 @@ import logging
 from PySide2 import QtWidgets, QtCore, QtGui
 import maya.cmds as cmds
 
-def random_scale():
-
-
-
-def random_rotate():
-    # print(random rotate)
-    # determine range of rotation from range of 0 to 360 in each axis
-    # determine position of vertice
-    # check determined rotation numbers
-    # Check if object is rotated
-    # If not then rotate based upon random number selection
-    # this will be a for loop
-
 
 def place_instances():
-    selected_verts = cmds.ls(flatten=True, orderedSelection=True)
+    selected_geo = cmds.ls(orderedSelection=True, flatten=True)
+    selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
+
     for vert in selected_verts:
-        # user can select an object to instance(grabs string name)
-        # create an instance
-        path_to_instance = cmds.instance('pCube1')
-        # move it to selected vert
+        # create a new instance based upon first selection
+        new_instance = cmds.instance(selected_geo)
+        # find the positions of selected vertices
         position = cmds.pointPosition(vert)
-        cmds.move(position[0], position[1], position[2], path_to_instance)
-        # call random_scale()
-        # call random_rotate()
+        # place the newly created instances onto the vertice positions
+        cmds.move(position[0], position[1], position[2], new_instance)
