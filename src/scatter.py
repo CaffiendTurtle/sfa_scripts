45455e0e4c8895ae03deb5c078a46f474a79eb0b
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 04:29:52 -0500

    Created variables that would allow the user to input a range for every axis for the rotation of their object

--- src/scatter.py
+++ src/scatter.py
@@ -5,12 +5,27 @@ import random
 import maya.cmds as cmds
 
 
-# def ran_rotation():
+# rotx = int(input())
+# roty = int(input())
+# rotz = int(input())
+
+
+# def ran_rotation(new_instance):
+
+
+
+# def ran_scaling():
 
 
 def place_instances():
     selected_geo = cmds.ls(orderedSelection=True, flatten=True)
     selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
+    fx_select = int(input())
+    sx_select = int(input())
+    fy_select = int(input())
+    sy_select = int(input())
+    fz_select = int(input())
+    sz_select = int(input())
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -20,9 +35,9 @@ def place_instances():
         cmds.move(position[0], position[1], position[2], new_instance)
         # creates random rotations for each instance created
         # need to create its own function so that I can just place that in
-        xRot = random.uniform(0, 360)
-        yRot = random.uniform(0, 360)
-        zRot = random.uniform(0, 360)
+        xRot = random.uniform(fx_select, sx_select)
+        yRot = random.uniform(fy_select, sy_select)
+        zRot = random.uniform(fz_select, sz_select)
         cmds.rotate(xRot, yRot, zRot, new_instance)
         # creates random scaling for each instance based upon a hard code range
         # needs to be placed in its own function and needs to be a for loop
@@ -30,4 +45,3 @@ def place_instances():
         scaling_Factor = random.uniform(100, 200)
         cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor,
                    new_instance)
-
