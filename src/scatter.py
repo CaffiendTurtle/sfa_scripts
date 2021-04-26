a0ed3e4dc07343e017b338a2456a20927ae3ca43
CaffiendTurtle <ep041998@gmail.com>
2021-04-25 19:34:30 -0500

    Started to bug out and not recognize the global parameter for min and max rotations. Works perfectly if placed inside the place_instances function.

--- src/scatter.py
+++ src/scatter.py
@@ -5,42 +5,36 @@ import random
 import maya.cmds as cmds
 
 
-def user_input():
-    minX_rotation = int(input())
-    maxX_rotation = int(input())
-    minY_rotation = int(input())
-    maxY_rotation = int(input())
-    minZ_rotation = int(input())
-    maxZ_rotation = int(input())
-    min_scale = int(input())
-    max_scale = int(input())
-
-    return minX_rotation, maxX_rotation, minY_rotation, maxY_rotation, minZ_rotation, maxZ_rotation, min_scale, max_scale
-
-def ran_rotation(new_instance,minX_rotation, maxX_rotation,
+def ran_rotation(new_instance, minX_rotation, maxX_rotation,
                  minY_rotation, maxY_rotation,
                  minZ_rotation, maxZ_rotation):
-
+    # stores the selected numbers from the user input
     xRot = random.uniform(minX_rotation, maxX_rotation)
     yRot = random.uniform(minY_rotation, maxY_rotation)
     zRot = random.uniform(minZ_rotation, maxZ_rotation)
     cmds.rotate(xRot, yRot, zRot, new_instance)
 
 
-def ran_scaling(new_instance, min_scale, max_scale):
-     # allows user to set their own min max scale
-     # needs to be placed in its own function and needs to be a for loop
+def ran_scaling(new_instance, min_size, max_size):
+    # allows user to set their own min max scale
+    # needs to be placed in its own function and needs to be a for loop
 
-     scaling_Factor = random.uniform(min_scale, max_scale)
-     cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor, new_instance)
+    scaling_Factor = random.uniform(min_size, max_size)
+    cmds.scale(scaling_Factor, scaling_Factor, scaling_Factor, new_instance)
 
 
 def place_instances():
     selected_geo = cmds.ls(orderedSelection=True, flatten=True)
     selected_verts = cmds.filterExpand(expand=True, selectionMask=31)
-    # min_scale = int(input())
-    # max_scale = int(input())
-    user_input()
+    # user_input()
+    minX_rotation = int(input())
+    maxX_rotation = int(input())
+    minY_rotation = int(input())
+    maxY_rotation = int(input())
+    minZ_rotation = int(input())
+    maxZ_rotation = int(input())
+    min_size = int(input())
+    max_size = int(input())
     for vert in selected_verts:
         # create a new instance based upon first selection
         new_instance = cmds.instance(selected_geo)
@@ -48,12 +42,9 @@ def place_instances():
         position = cmds.pointPosition(vert)
         # place the newly created instances onto the vertice positions
         cmds.move(position[0], position[1], position[2], new_instance)
-        # creates random rotations for each instance created
-        # user can create min max values for their rotation range per axis
-        # need to create its own function so that I can just place that in
+        # call random rotations for each instance
         ran_rotation(new_instance, minX_rotation, maxX_rotation,
                      minY_rotation, maxY_rotation,
                      minZ_rotation, maxZ_rotation)
-        #ran_scaling(new_instance, min_scale,max_scale)
 
-
+        ran_scaling(new_instance, min_size, max_size)
